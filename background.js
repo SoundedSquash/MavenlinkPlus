242c4687fd28879ae67531ce3571d52560b1e461
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2023-12-15 15:00:08 -0600

    Fix listener closed error on page load since we don't need to call it from sendMessage. It is done on page load in timesheets.js.

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
 chrome.webRequest.onCompleted.addListener(
   function(details) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      if (tabs[0] === undefined) return;
-      chrome.tabs.sendMessage(tabs[0].id, {action: "refreshTimesheet"}, function(response) {});  
+      if (tabs[0] === undefined || tabs[0].url.indexOf("timesheets") === -1) return;
+      chrome.tabs.sendMessage(tabs[0].id, {action: "refreshTimesheet"});  
     });
   },
-  {urls: ['*://*.mavenlink.com/timesheets'
-         ,'*://*.mavenlink.com/timesheets#'
-         ,'*://*.mavenlink.com/timesheets/time_entries*'
+  {urls: ['*://*.mavenlink.com/timesheets/time_entries*'
+         ,'*://*.kantata.com/timesheets/time_entries*'
          ,'*://*.mavenlink.com/timesheets/weekly*'
          ,'*://*.kantata.com/timesheets/weekly*']},
   []
