9fd08a320048e3a117977b6b28083c003400bb55
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2023-12-15 15:00:35 -0600

    Remove unused code.

--- background.js
+++ background.js
@@ -11,13 +11,3 @@ chrome.webRequest.onCompleted.addListener(
          ,'*://*.kantata.com/timesheets/weekly*']},
   []
 );
-// chrome.webRequest.onCompleted.addListener(
-//   function(details) {
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-//       chrome.tabs.sendMessage(tabs[0].id, {action: "refreshSchedule"}, function(response) {});  
-//     });
-//   },
-//   {urls: ['*://*.mavenlink.com/timesheets/time_entries/?tab=schedule*'
-//          ,'*://*.kantata.com/timesheets/weekly*']},
-//   []
-// );
